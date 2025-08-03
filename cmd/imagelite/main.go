149d0a2c6f34ce570e00f895dadc7c024510ad96
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 21:40:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/imagelite/main.go
@@ -0,0 +1,23 @@
+// cmd/imagelite/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"imagelite/internal/imagelite"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := imagelite.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
